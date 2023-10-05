1a109283435ad7d3876ba0a65c271e07c88ab050
mdiktushar <md.tushar1804@gmail.com>
2023-10-05 14:32:03 +0600

    tailwind css

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,5 @@
+@extends('Layout.mainLayout')
+@section('body')
+
+
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('Layout.mainLayout')
+@section('body')
+    <div class="text-center">
+        <h2 class="mb-5">Please login or register</h2>
+        <div class="flex gap-2 items-center justify-center">
+            <button class="btn btn-success">Login</button>
+            <button class="btn btn-info">Register</button>
+        </div>
+    </div>
+@endsection
