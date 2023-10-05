75ce35ffd05c52e79b037f464394771eb458168a
mdiktushar <md.tushar1804@gmail.com>
2023-10-05 18:08:51 +0600

    google recapture

--- resources/views/Layout/mainLayout.blade.php
+++ resources/views/Layout/mainLayout.blade.php
@@ -6,8 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Auth System</title>
-    {{-- <link href="https://cdn.jsdelivr.net/npm/daisyui@3.9.1/dist/full.css" rel="stylesheet" type="text/css" />
-    <script src="https://cdn.tailwindcss.com"></script> --}}
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <script src="/js/auth.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js" integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
     @vite('resources/css/app.css')
 </head>
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,54 @@
 @extends('Layout.mainLayout')
 @section('body')
+    <h1 class="text-center">Register</h1>
+    <form action="" class="flex flex-col items-center">
+        @csrf
+        <div class="form-control w-full max-w-xs">
+            <label class="label">
+                <span class="label-text">First Name</span>
+            </label>
+            <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+        </div>
+        <div class="form-control w-full max-w-xs">
+            <label class="label">
+                <span class="label-text">Last Name</span>
+            </label>
+            <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+        </div>
+        <div class="form-control w-full max-w-xs">
+            <label class="label">
+                <span class="label-text">Email</span>
+            </label>
+            <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+        </div>
+        <div class="form-control w-full max-w-xs">
+            <label class="label">
+                <span class="label-text">Password</span>
+            </label>
+            <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+        </div>
+        <div class="form-control w-full max-w-xs">
+            <label class="label">
+                <span class="label-text">Confirm Password</span>
+            </label>
+            <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+        </div>
+        <div class="g-recaptcha mt-5" data-sitekey="{{ env('reCAPTCHA_site_key') }}" data-callback="onCaptchaSuccess"
+            data-expired-callback="onCaptchaExpired">
+        </div>
+        <input type="hidden" name="recapture" id="recaptudre">
+        <button type="submit" class="btn btn-success mt-5">Success</button>
+    </form>
 
-register
+    <script>
+        function onCaptchaSuccess(response) {
+            var inputElement = document.getElementById("recaptudre");
+            inputElement.value = response;
+        }
+
+        function onCaptchaExpired() {
+            var inputElement = document.getElementById("recaptudre");
+            inputElement.value = '';
+        }
+    </script>
 @endsection
